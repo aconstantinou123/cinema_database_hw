0113990b5ff2261551454f7598e45b76531d1ba0
aconstantinou123 <a_constantinou@hotmail.co.uk>
2017-11-17 11:27:03 +0000

    Table, and classes made save function working

---
+++ models/customer.rb
@@ -0,0 +1,33 @@
+require_relative("../db/sql_runner")
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds'].to_i
+
+  end
+
+    def self.delete_all()
+      sql = 'DELETE FROM customers'
+      SqlRunner.run(sql)
+    end
+
+    def save()
+      sql = 'INSERT INTO customers (
+        name,
+        funds
+      ) VALUES (
+        $1,
+        $2
+        )
+        RETURNING *'
+      values = [@name, @funds]
+      @id = SqlRunner.run(sql, values)[0]['id'].to_i
+    end
+
+end
